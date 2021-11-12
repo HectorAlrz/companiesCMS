b4e2ef40e54459c801e308c59e9435d969c72663
Hector Hernandez Alcaraz <healcaraz@gmail.com>
2021-11-11 20:46:27 -0600

    Creating project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App mt-50px">
+      <h1 className='text-xl font-bold'>Companies CMS</h1>
+    </div>
+  );
+}
+
+export default App;
