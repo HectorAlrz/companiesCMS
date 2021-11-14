373f2a25421ff89f7bc7883b055285c2700998b8
Hector Alcaraz <healcaraz@gmail.com>
2021-11-13 19:08:24 -0600

    form

--- src/Pages/AddCompanie/index.js
+++ src/Pages/AddCompanie/index.js
@@ -3,6 +3,20 @@ import React from "react";
 export default function AddCompanie(){
 
     return(
-        <h3>Hello Addcompanie</h3>
+        <div className="flex justify-center text-center mt-50px">
+            <h3>Create a New Company</h3>
+            <div>
+
+            </div>
+            <div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+            
+        </div>
     )
 }
