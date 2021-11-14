f6aa34e3fa600d23d3054ca35f22a4487259112f
Hector Alcaraz <healcaraz@gmail.com>
2021-11-14 01:07:32 -0600

    warnings

--- src/Pages/CompanieList/index.js
+++ src/Pages/CompanieList/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import {
-    BrowserRouter as Router,
     Link,
 } from "react-router-dom"
 
@@ -18,7 +17,6 @@ export default function CompanieList() {
             const result = await getCompanies()
             setCompanieCards(result.data.companies)
             console.log('companies', result.data.companies)
-            console.log('estado', companieCards)
         }
         fetchData()
     }, [])
